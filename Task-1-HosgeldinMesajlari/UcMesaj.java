07e234c316ef52a1061e56500d86739b6c51fc8c
Hatice711 <h438406@gmail.com>
2025-10-21 15:48:30 +0300

    Update UcMesaj.java

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,7 @@
+public class UcMesaj {
+    public static void main(String[] args) {
+        System.out.println("Java'ya Hoş Geldin!");
+        System.out.println("Bilgisayar Bilimine Giriş");
+        System.out.println("Proglama Eğlencelidir.");
+    }
+}
