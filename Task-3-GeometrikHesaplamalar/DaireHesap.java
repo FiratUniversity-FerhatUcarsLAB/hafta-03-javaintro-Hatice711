b0b593fd13907b68990439c3af990ead69e2af43
Hatice711 <h438406@gmail.com>
2025-10-22 09:35:33 +0300

    Update DaireHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DaireHesap.java
@@ -0,0 +1,11 @@
+public class DaireHesap {
+    public static void main(String[] args) {
+        double r = (double)5.5F;
+        double pi = 3.14159;
+        double alan = pi * r * r;
+        double cevre = (double)2.0F * pi * r;
+        System.out.println(" Yaricap : " + r);
+        System.out.println(" Alan : " + alan);
+        System.out.println(" CEVRE : " + cevre);
+    }
+}
