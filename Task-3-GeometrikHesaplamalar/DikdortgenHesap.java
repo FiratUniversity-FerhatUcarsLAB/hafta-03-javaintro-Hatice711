19ff03a1a5ca9222c3c54668bb5a393fa846241d
Hatice711 <h438406@gmail.com>
2025-10-22 09:53:24 +0300

    Update DikdortgenHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,7 @@
+public class DikdortgenHesap {
+    public static void main(String[] args) {
+        double en = 4.5, boy = 7.9 ;
+        System.out.println("Alan:" +(en * boy));
+        System.out.println("Çevre:" + (2 * (boy * en)));
+    }
+}
