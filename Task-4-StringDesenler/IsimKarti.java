6888b82d23b17f33db00fd71255d01fd284839f2
Hatice711 <h438406@gmail.com>
2025-10-22 10:48:46 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,14 @@
+public class IsimKarti {
+    public static void main(String[] args) {
+        String ad = "Hatice";
+        String soyad = "Kırmızıgul";
+        String ogrNo = "250541102";
+        String bolum = "Yazılım Muhendisliği";
+
+        System.out.println("==== İSİM KARTI ====");
+        System.out.println("ad = " + ad);
+        System.out.println("soyad = " + soyad);
+        System.out.println("ogrNo = " + ogrNo);
+        System.out.println("bolum = " + bolum);
+    }
+}
