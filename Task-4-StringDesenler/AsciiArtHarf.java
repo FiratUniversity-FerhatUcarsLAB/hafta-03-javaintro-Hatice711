af3c431f21632ac7e127f94e078dae30e6922312
Hatice711 <h438406@gmail.com>
2025-10-22 10:56:04 +0300

    Update AsciiArtHarf.java

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,10 @@
+public class AsciiArtHarf {
+    public static void main(String[] args) {
+        System.out.println("==== ASCII ART HARF ====");
+        System.out.println("   H   ");
+        System.out.println("  H H  ");
+        System.out.println("  HHHH ");
+        System.out.println(" H   H ");
+        System.out.println(" H   H ");
+    }
+}
